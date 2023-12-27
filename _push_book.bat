c3ecc93e8b237c44f563cca1bf38ed2af22c6373
Lê Huỳnh Đức PTCL <duclh3@f88.vn>
2023-12-27 14:36:36 +0700

    push book

--- _push_book.bat
+++ _push_book.bat
@@ -1,4 +1,6 @@
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+
 git add .
 git commit -m "push book"
 git push
