948bedfbd08b5e078545cfa942f5fdf285593e11
Lê Huỳnh Đức PTCL <duclh3@f88.vn>
2024-01-18 16:03:51 +0700

    push book

--- _push_book.bat
+++ _push_book.bat
@@ -1,3 +1,4 @@
+git pull
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
--- notebooks/a.py
+++
@@ -1,6 +0,0 @@
-from statsmodels.tsa.ar_model import AutoReg
-import pandas as pd 
-from sklearn.metrics import mean_squared_error
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
